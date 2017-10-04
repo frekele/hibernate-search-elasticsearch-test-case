626a7d95d1396e14b15db778388f34e5473164c1
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-10-03 23:35:14 -0300

    improvement test case.

--- src/test/java/org/hibernate/search/bugs/FieldMappingTypeIT.java
+++ src/test/java/org/hibernate/search/bugs/FieldMappingTypeIT.java
@@ -2,12 +2,9 @@ package org.hibernate.search.bugs;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpHost;
 import org.apache.http.util.EntityUtils;
 import org.apache.lucene.search.Query;
 import org.elasticsearch.client.Response;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.search.FullTextSession;
@@ -19,8 +16,6 @@ import org.hibernate.search.testsupport.TestForIssue;
 import org.junit.After;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,76 +24,18 @@ import static org.junit.Assert.*;
 
 public class FieldMappingTypeIT extends SearchTestBase {
 
-    RestClient elasticsearchRestClient;
+    JsonNode responseJsonNode;
 
     @Override
     public Class<?>[] getAnnotatedClasses() {
         return new Class<?>[]{MyTestEntity.class};
     }
 
-    @Test
-    @TestForIssue(jiraKey = "HSEARCH-2908") // Please fill in the JIRA key of your issue
-    @SuppressWarnings("unchecked")
-    public void testHSEARCH2908() throws Exception {
-        try (Session s = getSessionFactory().openSession()) {
-            MyTestEntity yourEntity1 = new MyTestEntity(1L);
-
-            Transaction tx = s.beginTransaction();
-            s.persist(yourEntity1);
-            tx.commit();
-
-            FullTextSession session = Search.getFullTextSession(s);
-            QueryBuilder qb = session.getSearchFactory().buildQueryBuilder().forEntity(MyTestEntity.class).get();
-            Query query = qb.all().createQuery();
-            List<MyTestEntity> result = (List<MyTestEntity>) session.createFullTextQuery(query).list();
-            assertEquals(1, result.size());
-            assertEquals((long) 1, (long) result.get(0).getId());
-
-            Map<String, String> params = new HashMap<>();
-            params.put("pretty", "true");
-            Response response = elasticsearchRestClient.performRequest("GET", "/my-index/" + MyTestEntity.class.getCanonicalName() + "/_mapping", params);
-            String responseBody = EntityUtils.toString(response.getEntity());
-            System.out.println(responseBody);
-
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode responseNode = mapper.readTree(responseBody);
-
-            JsonNode jsonProperties = responseNode.get("my-index")
-                    .get("mappings")
-                    .get(MyTestEntity.class.getCanonicalName())
-                    .get("properties");
-
-            assertEquals("text", jsonProperties.get("stringWithAnalyze").get("type").textValue());
-            assertEquals("keyword", jsonProperties.get("stringWithoutAnalyze").get("type").textValue());
-            assertEquals("boolean", jsonProperties.get("booleanWrapWithAnalyze").get("type").textValue());
-            assertEquals("boolean", jsonProperties.get("booleanWrapWithoutAnalyze").get("type").textValue());
-            assertEquals("boolean", jsonProperties.get("booleanPrimitiveWithAnalyze").get("type").textValue());
-            assertEquals("boolean", jsonProperties.get("booleanPrimitiveWithoutAnalyze").get("type").textValue());
-            assertEquals("byte", jsonProperties.get("byteWrapWithAnalyze").get("type").textValue());
-            assertEquals("byte", jsonProperties.get("byteWrapWithoutAnalyze").get("type").textValue());
-            assertEquals("byte", jsonProperties.get("bytePrimitiveWithAnalyze").get("type").textValue());
-            assertEquals("byte", jsonProperties.get("bytePrimitiveWithoutAnalyze").get("type").textValue());
-            assertEquals("integer", jsonProperties.get("integerWrapWithAnalyze").get("type").textValue());
-            assertEquals("integer", jsonProperties.get("integerWrapWithoutAnalyze").get("type").textValue());
-            assertEquals("integer", jsonProperties.get("integerPrimitiveWithAnalyze").get("type").textValue());
-            assertEquals("integer", jsonProperties.get("integerPrimitiveWithoutAnalyze").get("type").textValue());
-            assertEquals("long", jsonProperties.get("longWrapWithAnalyze").get("type").textValue());
-            assertEquals("long", jsonProperties.get("longWrapWithoutAnalyze").get("type").textValue());
-            assertEquals("long", jsonProperties.get("longPrimitiveWithAnalyze").get("type").textValue());
-            assertEquals("long", jsonProperties.get("longPrimitiveWithoutAnalyze").get("type").textValue());
-            assertEquals("float", jsonProperties.get("floatWrapWithAnalyze").get("type").textValue());
-            assertEquals("float", jsonProperties.get("floatWrapWithoutAnalyze").get("type").textValue());
-            assertEquals("float", jsonProperties.get("floatPrimitiveWithAnalyze").get("type").textValue());
-            assertEquals("float", jsonProperties.get("floatPrimitiveWithoutAnalyze").get("type").textValue());
-            assertEquals("double", jsonProperties.get("doubleWrapWithAnalyze").get("type").textValue());
-            assertEquals("double", jsonProperties.get("doubleWrapWithoutAnalyze").get("type").textValue());
-            assertEquals("double", jsonProperties.get("doublePrimitiveWithAnalyze").get("type").textValue());
-            assertEquals("double", jsonProperties.get("doublePrimitiveWithoutAnalyze").get("type").textValue());
-            assertEquals("short", jsonProperties.get("shortWrapWithAnalyze").get("type").textValue());
-            assertEquals("short", jsonProperties.get("shortWrapWithoutAnalyze").get("type").textValue());
-            assertEquals("short", jsonProperties.get("shortPrimitiveWithAnalyze").get("type").textValue());
-            assertEquals("short", jsonProperties.get("shortPrimitiveWithoutAnalyze").get("type").textValue());
-        }
+    @Override
+    public void setUp() {
+        super.setUp();
+        this.createMapping();
+        this.loadResponseJsonNode();
     }
 
     @After
@@ -118,32 +55,130 @@ public class FieldMappingTypeIT extends SearchTestBase {
         }
     }
 
-    @Override
-    public void setUp() {
-        super.setUp();
-        this.buildElasticsearchRestClient();
-    }
+    @SuppressWarnings("unchecked")
+    private void createMapping() {
+        try (Session s = getSessionFactory().openSession()) {
+            MyTestEntity yourEntity1 = new MyTestEntity(1L);
 
-    @Override
-    public void tearDown() {
-        super.tearDown();
-        this.closeElasticsearchRestClient();
-    }
+            Transaction tx = s.beginTransaction();
+            s.persist(yourEntity1);
+            tx.commit();
 
-    public void buildElasticsearchRestClient() {
-        String protocol = "http";
-        String url = InetAddress.getLoopbackAddress().getHostAddress();
-        String port = "9200";
-        System.out.println("RestClient: " + protocol + "://" + url + ":" + port);
-        RestClientBuilder builder = RestClient.builder(new HttpHost(url, Integer.valueOf(port), protocol));
-        this.elasticsearchRestClient = builder.build();
-    }
-
-    public void closeElasticsearchRestClient() {
-        try {
-            elasticsearchRestClient.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            FullTextSession session = Search.getFullTextSession(s);
+            QueryBuilder qb = session.getSearchFactory().buildQueryBuilder().forEntity(MyTestEntity.class).get();
+            Query query = qb.all().createQuery();
+            List<MyTestEntity> result = (List<MyTestEntity>) session.createFullTextQuery(query).list();
+            assertEquals(1, result.size());
+            assertEquals((long) 1, (long) result.get(0).getId());
         }
+    }
+
+    private void loadResponseJsonNode() {
+        try {
+            Map<String, String> params = new HashMap<>();
+            params.put("pretty", "true");
+            Response response = getElasticsearchRestClient().performRequest("GET", "/my-index/" + MyTestEntity.class.getCanonicalName() + "/_mapping", params);
+            String responseBody = EntityUtils.toString(response.getEntity());
+            System.out.println(responseBody);
+
+            ObjectMapper mapper = new ObjectMapper();
+            this.responseJsonNode = mapper.readTree(responseBody);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    @TestForIssue(jiraKey = "HSEARCH-2908")
+    @SuppressWarnings("unchecked")
+    public void testStringField() throws Exception {
+        JsonNode jsonProperties = responseJsonNode.get("my-index").get("mappings")
+                .get(MyTestEntity.class.getCanonicalName()).get("properties");
+        assertEquals("text", jsonProperties.get("stringWithAnalyze").get("type").textValue());
+        assertEquals("keyword", jsonProperties.get("stringWithoutAnalyze").get("type").textValue());
+    }
+
+    @Test
+    @TestForIssue(jiraKey = "HSEARCH-2908")
+    @SuppressWarnings("unchecked")
+    public void testBooleanField() throws Exception {
+        JsonNode jsonProperties = responseJsonNode.get("my-index").get("mappings")
+                .get(MyTestEntity.class.getCanonicalName()).get("properties");
+        assertEquals("boolean", jsonProperties.get("booleanWrapWithAnalyze").get("type").textValue());
+        assertEquals("boolean", jsonProperties.get("booleanWrapWithoutAnalyze").get("type").textValue());
+        assertEquals("boolean", jsonProperties.get("booleanPrimitiveWithAnalyze").get("type").textValue());
+        assertEquals("boolean", jsonProperties.get("booleanPrimitiveWithoutAnalyze").get("type").textValue());
+    }
+
+    @Test
+    @TestForIssue(jiraKey = "HSEARCH-2908")
+    @SuppressWarnings("unchecked")
+    public void testByteField() throws Exception {
+        JsonNode jsonProperties = responseJsonNode.get("my-index").get("mappings")
+                .get(MyTestEntity.class.getCanonicalName()).get("properties");
+        assertEquals("byte", jsonProperties.get("byteWrapWithAnalyze").get("type").textValue());
+        assertEquals("byte", jsonProperties.get("byteWrapWithoutAnalyze").get("type").textValue());
+        assertEquals("byte", jsonProperties.get("bytePrimitiveWithAnalyze").get("type").textValue());
+        assertEquals("byte", jsonProperties.get("bytePrimitiveWithoutAnalyze").get("type").textValue());
+    }
+
+    @Test
+    @TestForIssue(jiraKey = "HSEARCH-2908")
+    @SuppressWarnings("unchecked")
+    public void testIntegerField() throws Exception {
+        JsonNode jsonProperties = responseJsonNode.get("my-index").get("mappings")
+                .get(MyTestEntity.class.getCanonicalName()).get("properties");
+        assertEquals("integer", jsonProperties.get("integerWrapWithAnalyze").get("type").textValue());
+        assertEquals("integer", jsonProperties.get("integerWrapWithoutAnalyze").get("type").textValue());
+        assertEquals("integer", jsonProperties.get("integerPrimitiveWithAnalyze").get("type").textValue());
+        assertEquals("integer", jsonProperties.get("integerPrimitiveWithoutAnalyze").get("type").textValue());
+    }
+
+    @Test
+    @TestForIssue(jiraKey = "HSEARCH-2908")
+    @SuppressWarnings("unchecked")
+    public void testLongField() throws Exception {
+        JsonNode jsonProperties = responseJsonNode.get("my-index").get("mappings")
+                .get(MyTestEntity.class.getCanonicalName()).get("properties");
+        assertEquals("long", jsonProperties.get("longWrapWithAnalyze").get("type").textValue());
+        assertEquals("long", jsonProperties.get("longWrapWithoutAnalyze").get("type").textValue());
+        assertEquals("long", jsonProperties.get("longPrimitiveWithAnalyze").get("type").textValue());
+        assertEquals("long", jsonProperties.get("longPrimitiveWithoutAnalyze").get("type").textValue());
+    }
+
+    @Test
+    @TestForIssue(jiraKey = "HSEARCH-2908")
+    @SuppressWarnings("unchecked")
+    public void testFloatField() throws Exception {
+        JsonNode jsonProperties = responseJsonNode.get("my-index").get("mappings")
+                .get(MyTestEntity.class.getCanonicalName()).get("properties");
+        assertEquals("float", jsonProperties.get("floatWrapWithAnalyze").get("type").textValue());
+        assertEquals("float", jsonProperties.get("floatWrapWithoutAnalyze").get("type").textValue());
+        assertEquals("float", jsonProperties.get("floatPrimitiveWithAnalyze").get("type").textValue());
+        assertEquals("float", jsonProperties.get("floatPrimitiveWithoutAnalyze").get("type").textValue());
+    }
+
+    @Test
+    @TestForIssue(jiraKey = "HSEARCH-2908")
+    @SuppressWarnings("unchecked")
+    public void testDoubleField() throws Exception {
+        JsonNode jsonProperties = responseJsonNode.get("my-index").get("mappings")
+                .get(MyTestEntity.class.getCanonicalName()).get("properties");
+        assertEquals("double", jsonProperties.get("doubleWrapWithAnalyze").get("type").textValue());
+        assertEquals("double", jsonProperties.get("doubleWrapWithoutAnalyze").get("type").textValue());
+        assertEquals("double", jsonProperties.get("doublePrimitiveWithAnalyze").get("type").textValue());
+        assertEquals("double", jsonProperties.get("doublePrimitiveWithoutAnalyze").get("type").textValue());
+    }
+
+    @Test
+    @TestForIssue(jiraKey = "HSEARCH-2908")
+    @SuppressWarnings("unchecked")
+    public void testShortField() throws Exception {
+        JsonNode jsonProperties = responseJsonNode.get("my-index").get("mappings")
+                .get(MyTestEntity.class.getCanonicalName()).get("properties");
+        assertEquals("short", jsonProperties.get("shortWrapWithAnalyze").get("type").textValue());
+        assertEquals("short", jsonProperties.get("shortWrapWithoutAnalyze").get("type").textValue());
+        assertEquals("short", jsonProperties.get("shortPrimitiveWithAnalyze").get("type").textValue());
+        assertEquals("short", jsonProperties.get("shortPrimitiveWithoutAnalyze").get("type").textValue());
     }
 }
--- src/test/java/org/hibernate/search/bugs/SearchTestBase.java
+++ src/test/java/org/hibernate/search/bugs/SearchTestBase.java
@@ -1,5 +1,8 @@
 package org.hibernate.search.bugs;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
@@ -9,38 +12,65 @@ import org.hibernate.service.spi.ServiceRegistryImplementor;
 import org.junit.After;
 import org.junit.Before;
 
+import java.io.IOException;
+import java.net.InetAddress;
+
 public abstract class SearchTestBase {
-	
-	private SessionFactory sessionFactory;
-	
-	@Before
-	public void setUp() {
-		StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
-		ServiceRegistryImplementor serviceRegistry = (ServiceRegistryImplementor) registryBuilder.build();
 
-		MetadataSources ms = new MetadataSources( serviceRegistry );
-		Class<?>[] annotatedClasses = getAnnotatedClasses();
-		if ( annotatedClasses != null ) {
-			for ( Class<?> entity : annotatedClasses ) {
-				ms.addAnnotatedClass( entity );
-			}
-		}
+    private SessionFactory sessionFactory;
 
-		Metadata metadata = ms.buildMetadata();
+    private RestClient elasticsearchRestClient;
 
-		final SessionFactoryBuilder sfb = metadata.getSessionFactoryBuilder();
-		this.sessionFactory = sfb.build();
-	}
-	
-	@After
-	public void tearDown() {
-		this.sessionFactory.close();
-	}
+    @Before
+    public void setUp() {
+        StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+        ServiceRegistryImplementor serviceRegistry = (ServiceRegistryImplementor) registryBuilder.build();
 
-	protected abstract Class<?>[] getAnnotatedClasses();
-	
-	protected SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
+        MetadataSources ms = new MetadataSources(serviceRegistry);
+        Class<?>[] annotatedClasses = getAnnotatedClasses();
+        if (annotatedClasses != null) {
+            for (Class<?> entity : annotatedClasses) {
+                ms.addAnnotatedClass(entity);
+            }
+        }
 
+        Metadata metadata = ms.buildMetadata();
+
+        final SessionFactoryBuilder sfb = metadata.getSessionFactoryBuilder();
+        this.sessionFactory = sfb.build();
+        this.buildElasticsearchRestClient();
+    }
+
+    @After
+    public void tearDown() {
+        this.sessionFactory.close();
+        this.closeElasticsearchRestClient();
+    }
+
+    private void buildElasticsearchRestClient() {
+        String protocol = "http";
+        String url = InetAddress.getLoopbackAddress().getHostAddress();
+        String port = "9200";
+        System.out.println("RestClient: " + protocol + "://" + url + ":" + port);
+        RestClientBuilder builder = RestClient.builder(new HttpHost(url, Integer.valueOf(port), protocol));
+        this.elasticsearchRestClient = builder.build();
+    }
+
+    private void closeElasticsearchRestClient() {
+        try {
+            elasticsearchRestClient.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected abstract Class<?>[] getAnnotatedClasses();
+
+    protected SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public RestClient getElasticsearchRestClient() {
+        return elasticsearchRestClient;
+    }
 }
